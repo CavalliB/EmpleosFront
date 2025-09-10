66d734490f470f4770debe9becd43973301367cc
CavalliB <Cerdopigman12@hotmail.com>
2025-09-10 18:35:42 -0300

    db.json y controladores de bd

---
+++ src/services/ad.jsx
@@ -0,0 +1,25 @@
+import axios from "axios";
+const localUrl = "/ad";
+
+const getAll = () => {
+  const request = axios.get(localUrl);
+  return request.then((response) => response.data);
+};
+
+const create = (newObject) => {
+  const request = axios.post(localUrl, newObject);
+  return request.then((response) => response.data);
+};
+
+const update = (id, newObject) => {
+  const request = axios.put(`${localUrl}/${id}`, newObject);
+  return request.then((response) => response.data);
+};
+
+const deletead = (id) => {
+  const request = axios.delete(`${localUrl}/${id}`);
+  return request.then((response) => response.data);
+};
+
+export default { getAll, create, update, deletead };
+
